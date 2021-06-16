9e852110df0e0433a78da3be30b0fba87691e325
Thomas <thomascasale06@gmail.com>
2021-06-16 23:10:25 +0200

    add homework

--- C07/ex00/ft_strdup.c
+++ C07/ex00/ft_strdup.c
@@ -1,6 +1,44 @@
 #include <stdlib.h>
 
+int	ft_strlen(char *str)
+{
+	int	n;
+
+	n = 0;
+	while (str[n])
+		n++;
+	return (n);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] || dest[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
 char	*ft_strdup(char *src)
 {
-	
+	char *dest;
+	dest = malloc(sizeof(char ) * ft_strlen(src));
+	if (!dest)
+		return (NULL);
+	return (ft_strcpy(dest, src));
 }
+/*
+#include <stdio.h>
+
+int	main()
+{
+	char	src[] = "pute";
+	printf("%s\n", ft_strdup(src));
+	return (0);
+}
+*/
---
+++ C08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "ft_stock_str.h"
+
+struct	s_stock_str	*ft_strs_to_tab(int ac, char **av);
+{
+	while (ac => 0)
+	{
+		av[ac] == s_stock_str;
+	}
+};
